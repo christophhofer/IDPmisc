a0fd78af988b44652ad2bb04caac4f75235c3ffb
locherx <locherx@gmail.com>
2020-01-21 14:05:48 +0100

    V1.1.20: DAAG link removed in help.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 REM build.bat
 
-set R=R-3.5.1
+set R=R-3.6.1
 set pckg=IDPmisc
-set version=1.1.19
+set version=1.1.20
 
 setlocal
 path %path%;"C:\Program Files\R\%R%\bin\x64";"C:\Program Files\R\%R%\bin\i386"
